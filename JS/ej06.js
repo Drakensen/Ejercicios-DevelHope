72195ca2cc4735cea0f76306fcc87b9a5a45fe60
Pablo <pablofergon92@gmail.com>
2024-02-01 13:10:31 +0100

    Ejercicios JS

--- JS/ej06.js
+++ JS/ej06.js
@@ -2,13 +2,13 @@
 the value of this variable will be: "Hello John". Also declare a function named inner and return the helloName variable. 
 The purpose is printing "Hello John" in the console. */
 
-function helloName (callback) {
-    console.log("Hello");
-    callback();
+function printName () {  //Crea una Función.
+    let helloName = "Hello John"; //Crea una variable.
+    
+    function inner() { //Aquí creas una segunda función dentro de la primera función.
+        console.log(helloName);
+    }
+    return inner; //Aquí llamas a la segunda función.
 }
 
-function printName () {
-        console.log("Jhon");
-}
-
-helloName(printName);
+printName()(); //Aquí muestra por pantalla a la primera función (que realmente está dando la segunda, que es la única que hace algo funcional).
