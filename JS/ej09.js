e033928e3e722504f2e1eb63f7a787be126f2047
Pablo <pablofergon92@gmail.com>
2024-02-02 10:45:27 +0100

    Ejercicios JS

--- JS/ej09.js
+++ JS/ej09.js
@@ -1,9 +1,18 @@
 /* Create an object person. Print its key/value pair in the console. Try to use the method Object.keys */
 
-let person = {
+const person = {
     firstName: "Mario",
     lastName: "Rossi",
     age: 25
 }
 
-console.log(Object.keys(person));
+function printObject (obj) {
+    const keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const value = obj[key];
+        console.log(`${key}: ${value}`);
+    }
+}
+    
+printObject(person);
