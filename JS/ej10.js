74ea3e2710e1d01a53f11eb3d0fa12b457e96df5
Pablo <pablofergon92@gmail.com>
2024-02-02 10:48:21 +0100

    Ejercicios JS

--- JS/ej10.js
+++ JS/ej10.js
@@ -2,14 +2,6 @@
 
 const order = {};
 
-if (
-    order && 
-    order.customer && 
-    order.customer.address && 
-    !order.customer.address.city
-    ) {
-        console.log('City is required');
-    }
-
-const optionalChaining = !order?.customer?.address?.city || "City is required";
-console.log(optionalChaining);
+if (!order?.customer?.address?.city) {
+  console.log('City is required');
+}
