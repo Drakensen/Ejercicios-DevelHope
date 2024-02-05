0200060588c2a6ef87a5f348505fd32d2746cf38
Pablo <pablofergon92@gmail.com>
2024-02-05 20:32:00 +0100

    Ejercicios JS

--- 3.-JavaScript/ej26.js
+++ 3.-JavaScript/ej26.js
@@ -4,5 +4,5 @@ function sum(... numbers) {
     return numbers.reduce ((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
-console.log(sum(10+20+3+5+2+8+2+4+3+1+9+21+5+7)); //El ejercicio pide que pueda tener cualquier número de parámetros para sumar.
-console.log(sum(3+2+5)); //Aquí otro ejemplo de que el código funciona, con menos de cinco parámetros.
+console.log(sum(10,20,3,5,2,8,2,4,3,1,9,21,5,7)); //El ejercicio pide que pueda tener cualquier número de parámetros para sumar.
+console.log(sum(3,2,5)); //Aquí otro ejemplo de que el código funciona, con menos de cinco parámetros.
