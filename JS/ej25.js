6b3dce748d2f8e53e52078b6deec1e47dbaf7061
Pablo <pablofergon92@gmail.com>
2024-02-05 12:27:23 +0100

    Ejercicios JS

--- JS/ej25.js
+++ JS/ej25.js
@@ -1,6 +1,8 @@
 /* Do you know how to "merge" the value of the newNumber variable into numberStore without using the array method push? */
 
 let numberStore = [0, 1, 2];
-numberStore[numberStore.length] = 3;
+let newNumber = 3;
 
+//numberStore.push(newNumber); Se sustituye el push por la línea de abajo.
+numberStore = [...numberStore, newNumber];
 console.log(numberStore);
