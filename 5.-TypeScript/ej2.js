e43e59f0799aa23ff4150c8a7a431fcefb3f092f
Pablo <pablofergon92@gmail.com>
2024-04-09 16:06:30 +0200

    Ejercicios TypeScript

--- 5.-TypeScript/ej2.js
+++ 5.-TypeScript/ej2.js
@@ -1,42 +1,26 @@
+"use strict";
 /**
  * START: Follow the instructions below.
  */
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Add types to this function declaration.
-
-function doubleThePopulation(value: number): number {
+function doubleThePopulation(value) {
     return value * 2;
 }
-
 // Correct or remove the function calls which cause type errors.
-
 doubleThePopulation(5);
-
 doubleThePopulation(6);
-
 doubleThePopulation(8);
-
 doubleThePopulation(0);
-
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
-
-function languagesSpoken(country: string, language1: string, language2: any): void {
+function languagesSpoken(country, language1, language2) {
     console.log(`The languages spoken in ${country} are:`);
-
     console.log(language1);
-
-    if (language2 !== undefined){
+    if (language2 !== undefined) {
         console.log(language2);
     }
 }
-
 languagesSpoken("Colombia", "Spanish", "English");
-
 languagesSpoken("Greece", "Greek", "English");
-
 languagesSpoken("New Zealand", "English", "Māori");
-
-// ----
-
-export {};
--- 5.-TypeScript/ej5.js
+++ 5.-TypeScript/ej5.js
@@ -1,34 +1,22 @@
+"use strict";
 /**
  * START: Follow the instructions below.
  */
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Add the `any` type to fix the type errors in the following code.
-
 let currency = { name: "Indian rupee", any: "" };
-
 currency.name = "THB";
-
 currency.name = "Baht";
-
 // Fix the if statement in this function so the type of `value` is narrowed to `string`.
-
-function countryNameLength(string: unknown) {
+function countryNameLength(string) {
     if (typeof string === "string") {
         console.log(string.length);
     }
 }
-
 countryNameLength("United States of America");
-
 // Fix the type error we see when calling this function.
 // Hint: Replace one of the `never` types with a different type.
-
-function throwCountryError(message: string): never {
+function throwCountryError(message) {
     throw new Error(`Could not find country: ${message}`);
 }
-
 throwCountryError("Narnia");
-
-// ----
-
-export {};
