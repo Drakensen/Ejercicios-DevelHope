da95ebc53e463ee9a0fb73034a07526948c0dca8
Pablo <pablofergon92@gmail.com>
2024-04-12 14:32:24 +0200

    Ejercicios TypeScript

--- 5.-TypeScript/Ej4.ts
+++ 5.-TypeScript/Ej4.ts
@@ -2,6 +2,8 @@
  * START: Follow the instructions below.
  */
 
+import { Type } from "typescript";
+
 interface Country {
     name: string;
     code: string;
@@ -17,7 +19,7 @@ interface CountryLanguages {
 
 // Change the `CountryWithLanguages` type into an intersection type that uses the interfaces defined above. This should fix the type error on Line 28.
 
-type CountryWithLanguages = Country;
+type CountryWithLanguages = any;
 
 const countryA: CountryWithLanguages = {
     name: "Greece",
@@ -26,13 +28,16 @@ const countryA: CountryWithLanguages = {
 };
 
 // Create an intersection type named `CountryWithStatistics` that describes the shape of the object below. Use the interfaces defined above.
-// Use the type alias syntax: type CountryWithStatistics = type;
+// Use the type alias syntax: type CountryWithStatistics = Type;
 // Add a type annotation with the `CountryWithStatistics` type on the variable `countryB`.
+
+type CountryWithStatistics = Type;
 
 const countryB = {
     name: "China",
     code: "CN",
-    population: 1_412_600_000
+    population: 1_412_600_000,
+    languages: ["Chinese"]
 };
 
 // ----
