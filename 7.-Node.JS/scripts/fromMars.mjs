8ab15f6d8509f0c7fad9e522c0c5b235b63929b3
Pablo <pablofergon92@gmail.com>
2024-04-24 11:46:37 +0200

    Ejercicios Node.JS

--- 7.-Node.JS/scripts/fromMars.mjs
+++ 7.-Node.JS/scripts/fromMars.mjs
@@ -7,7 +7,7 @@ const server = createServer ((request, response) => {
 
     response.setHeader("Content-Type", "application/json")
 
-    const jsonResponseBody = JSON.stringify ({ location: "Mars"})
+    const jsonResponseBody = JSON.stringify ({ location: "Mars" })
 
     response.end(jsonResponseBody)
 })
