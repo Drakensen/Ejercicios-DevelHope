81685671773875a243e648a39dcb14f00aca2dec
Pablo Fernández <pablofergon92@gmail.com>
2024-05-02 16:58:11 +0200

    Ejercicios Node.JS

--- 7.-Node.JS/scripts/planetsController.js
+++ 7.-Node.JS/scripts/planetsController.js
@@ -38,32 +38,32 @@ const getOneById = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
     res.status(200).json(planet);
 });
 exports.getOneById = getOneById;
-const create = (req, res) => {
-    const { error } = planetSchema.validate(req.body);
-    if (error) {
-        return res.status(400).send({ message: "Invalid request body", error: error.details });
+const create = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name } = req.body;
+    const newPlanet = { name };
+    const validateNewPlanet = planetSchema.validate(newPlanet);
+    if (validateNewPlanet.error) {
+        return res
+            .status(400)
+            .json({ msg: validateNewPlanet.error.details[0].message });
     }
-    const { id, name } = req.body;
-    const newPlanet = { id, name };
-    planets = [...planets, newPlanet];
+    else {
+        yield getPlanets_1.db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
+        res.status(201).json({ msg: "The planet was created" });
+    }
     res.status(201).json(newPlanet);
-};
+});
 exports.create = create;
-const updateById = (req, res) => {
-    const { error } = planetSchema.validate(req.body);
-    if (error) {
-        return res.status(400).send({ message: "Invalid request body", error: error.details });
-    }
+const updateById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { name } = req.body;
-    planets = planets.map(p => p.id === Number(id) ? (Object.assign(Object.assign({}, p), { name })) : p);
-    console.log(planets);
+    yield getPlanets_1.db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
     res.status(200).json({ msg: "The planet was updated" });
-};
+});
 exports.updateById = updateById;
-const deleteById = (req, res) => {
+const deleteById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    planets = planets.filter((p) => p.id !== Number(id));
+    yield getPlanets_1.db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
     res.status(200).json({ msg: "The planet was deleted" });
-};
+});
 exports.deleteById = deleteById;
--- 7.-Node.JS/scripts/planetsController.ts
+++ 7.-Node.JS/scripts/planetsController.ts
@@ -39,36 +39,34 @@ const getOneById = async(req: Request, res: Response) => {
 }
 
 const create = async(req: Request, res: Response) => {
-    const { error } = planetSchema.validate(req.body)
-    if (error) {
-        return res.status(400).send({ message: "Invalid request body", error: error.details })
-    }
 
-    const { id, name } = req.body as Planet
-    const newPlanet = { id, name }
-    const planet = await db.one(`INSERT * FROM planets WHERE id=$1`, Number(id))
+    const { name } = req.body as Planet
+    const newPlanet = {  name }
+    const validateNewPlanet = planetSchema.validate(newPlanet)
+
+    if (validateNewPlanet.error) {
+        return res
+            .status(400)
+            .json({ msg: validateNewPlanet.error.details[0].message })
+    } else {
+        await db.none(`INSERT INTO planets (name) VALUES ($1)`, name)
+        res.status(201).json({ msg: "The planet was created" })
+    }
     res.status(201).json(newPlanet)
 }
 
-const updateById = (req: Request, res: Response) => {
-    const { error } = planetSchema.validate(req.body)
-    if (error) {
-        return res.status(400).send({ message: "Invalid request body", error: error.details })
-    }
+const updateById = async(req: Request, res: Response) => {
+    const { id } = req.params
+    const { name } = req.body
 
-    const {id} = req.params
-    const {name} = req.body
-    planets = planets.map (p => p.id === Number (id) ? ({...p, name}) : p)
+    await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name])
 
-    console.log(planets)
-
-    res.status(200).json({msg: "The planet was updated"})
+    res.status(200).json({ msg: "The planet was updated" })
 }
 
-const deleteById = (req: Request, res: Response) => {
-    const {id} = req.params
-
-    planets = planets.filter((p) => p.id !== Number(id))
+const deleteById = async(req: Request, res: Response) => {
+    const { id } = req.params
+    await db.none(`DELETE FROM planets WHERE id=$1`, Number(id))
 
     res.status(200).json({ msg: "The planet was deleted" })
 }
