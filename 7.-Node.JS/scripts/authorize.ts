8565e736fcc58773e1b25a898469c5adcc09da93
Pablo Fernández <pablofergon92@gmail.com>
2024-05-07 16:06:02 +0200

    Ejercicios Node.JS

--- 7.-Node.JS/scripts/authorize.ts
+++ 7.-Node.JS/scripts/authorize.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from "express"
+import passport from 'passport'
 import jwt from 'jsonwebtoken'
 
 const authorize = async (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate("jwt", {session: false}, (err, user) => {
+    passport.authenticate ("jwt", {session: false}, (err, user) => {
         if (!user || err) {
             res.status(401).json({ msg: "Unathorized." })
         } else {
